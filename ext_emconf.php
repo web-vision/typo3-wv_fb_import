1fcf4c6a704e660a27c25cd486b23e8d61cc1afe
Daniel Siepmann <d.siepmann@web-vision.de>
2015-09-14 17:05:26 +0200

    [WIP][FEATURE] Fetch posts from FB
    * Fetch posts from a single facebook page by there id / name using an
      access_token
    
    Related: IYR-772-93223

---
+++ ext_emconf.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Open Graph Import',
+    'description' => 'Enables import of Facebook Open Graph information.',
+    'category' => 'backend',
+    'version' => '0.0.1',
+    'state' => 'alpha',
+    'clearcacheonload' => false,
+    'author' => 'Daniel Siepmann',
+    'author_email' => 'd.siepmann@web-vision.de',
+    'author_company' => 'web-vision.de',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-6.2.99',
+            'scheduler' => '',
+        ),
+        'suggests' => array(
+            'news' => '',
+        ),
+    ),
+);
